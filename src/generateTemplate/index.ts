99bb132b939acecfc4b7705e803f676d7d6a0b67
Simon Iribarren <simon.godoy@check24.de>
2019-10-17 11:05:13 +0200

    open browser automatically

--- src/generateTemplate/index.ts
+++ src/generateTemplate/index.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import { join } from 'path';
 import { BundlesReportI } from '../types';
+import * as open from 'open';
 
 
 
@@ -10,5 +11,9 @@ export function runResultExpress(bundleReport: BundlesReportI) {
     app.set('view engine', 'ejs');
 
     app.get('/', (req: express.Request, res: express.Response) => res.render('result', { bundleReport }));
-    const server = app.listen(3333, () => console.log('open http://localhost:3333/ in your browser'));
+    const server = app.listen(3333, () => {
+        console.log('open http://localhost:3333/ in your browser');
+        open('http://localhost:3333/');
+    });
+
 }
